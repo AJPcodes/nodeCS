3a329ae5fd790b9c35a6e6834b35597d201679f6
AJP <allenatmc@gmail.com>
2016-03-07 14:39:06 -0600

    set up for basic single-linked nodes

---
+++ list.js
@@ -0,0 +1,91 @@
+"use strict"
+
+class Node {
+  constructor(data) {
+    this.payload = data
+    this.next = null
+
+  }
+}
+
+// // last in first out (stack of plates)
+// class List {
+//   constructor() {
+//     this.head = null
+//   }
+
+//   add (node) {
+//     node.next = this.head
+
+//     if (!this.head) {
+//       this.head = node
+//     }
+
+//   }
+
+// }
+
+// // n interations
+// class List {
+//   constructor() {
+//     this.head = null
+//   }
+
+//   add (node) {
+//     //first item
+//     if (!this.head) {
+//       this.head = node
+//     }
+//     //nth (non zero) item
+//     else {
+
+//       var currentHead = this.head
+
+//       while (currentHead.next) {
+//         currentHead = currentHead.next
+//       }
+
+//       currentHead.next = node
+
+//     }
+
+
+
+//   }
+
+// }
+
+
+class List {
+  constructor() {
+    this.head = null
+    this.tail = null
+  }
+
+  add (node) {
+    //first item
+    if (!this.head) {
+      this.head = node
+      this.tail = node
+    }
+    //nth (non zero) item
+    else {
+
+      this.tail.next = node
+      this.tail = node
+
+    }
+
+
+  }
+
+}
+
+const myList = new List()
+myList.add(new Node("a"))
+myList.add(new Node("b"))
+myList.add(new Node("c"))
+myList.add(new Node("d"))
+myList.add(new Node("e"))
+
+console.log(myList)
