f10b78bc42d8b91e64c4e62704afbe102bad454c
AJP <allenatmc@gmail.com>
2016-03-09 15:05:25 -0600

    add quicksort example

--- sort.js
+++ sort.js
@@ -113,4 +113,59 @@ const selectionSort = (array) => {
 }
 
 
-console.log(selectionSort(shuffled))
+
+const partition = (items, left, right) => {
+
+    var pivot   = items[Math.floor((right + left) / 2)],
+        i       = left,
+        j       = right;
+
+
+    while (i <= j) {
+
+        while (items[i] < pivot) {
+            i++;
+        }
+
+        while (items[j] > pivot) {
+            j--;
+        }
+
+        if (i <= j) {
+            swap(items, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    return i;
+}
+
+const quickSort = (items, left, right) => {
+
+    var index;
+
+    if (items.length > 1) {
+
+        left = typeof left != "number" ? 0 : left;
+        right = typeof right != "number" ? items.length - 1 : right;
+
+        index = partition(items, left, right);
+
+        if (left < index - 1) {
+            quickSort(items, left, index - 1);
+        }
+
+        if (index < right) {
+            quickSort(items, index, right);
+        }
+
+    }
+
+    return items;
+}
+
+
+
+
+console.log(quickSort(shuffled))
