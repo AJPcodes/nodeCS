525b9fba29af4be08531d5b74de813d8d3fde939
AJP <allenatmc@gmail.com>
2016-03-09 10:48:34 -0600

    add chalk

--- sort.js
+++ sort.js
@@ -2,25 +2,30 @@
 
 const _ = require('lodash')
 const arr = _.range(1,10)
+const chalk = require('chalk')
+const util = require('util')
+
+
 let shuffled = _.shuffle(arr)
+let loop1 = 0
+let loop2 = 0
 
 
 const swap = (array, index1, index2) => {
-  console.log(array)
   const temp = array[index1]
-
   array[index1] = array[index2]
   array[index2] = temp
 
 }
 
 //basic bubble sort
-function bubbleSort(array) {
+function bubbleSort1 (array) {
 
   let sorted = false
 
   while (!sorted) {
-
+    loop1++
+    console.log(chalk.red(util.inspect(array)), loop1)
     sorted = true
 
     for (let i = 0; i < array.length -1; i++) {
@@ -33,9 +38,22 @@ function bubbleSort(array) {
 
     }
   }
-
   return array;
-
 }
 
-console.log(bubbleSort(shuffled));
+function bubbleSort2 (array) {
+
+  for (let j = 1; j < array.length; j++) {
+    // loop2++
+    // console.log('loop2', loop2)
+    for (let i = 0; i < array.length - j; i++) {
+      if (array[i] > array[i + 1]) {
+        swap(array, i, i + 1)
+      }
+    }
+  }
+  return array
+}
+
+console.log(bubbleSort1(shuffled))
+console.log(bubbleSort2(shuffled))
